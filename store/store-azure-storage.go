eb3a3d51cf15c4006802a6ec4ca32c51676dc763
brm_ <brm.ryd@gmail.com>
2020-09-28 13:16:45 +0700

    update storage modules

--- store/store-azure-storage.go
+++ store/store-azure-storage.go
@@ -23,6 +23,45 @@ type AzureStorage struct {
 }
 
 // Actions (like in store modules interfaces)
+func (f *AzureStorage) Set(name string, in io.Reader, tag interface{}) (tagOut interface{}, err error) {
+	// Create blob URL
+	u, err := url.Parse(f.storageURL + "/" + name)
+	if err != nil {
+		return
+	}
+	blockBlobURL := azblob.NewBlockBlobURL(*u, f.storagePipeline)
+
+}
+
 func (f *AzureStorage) Get(name string, out io.Writer) (found bool, tag interface{}, err error) {
+	found = true
+
+	// Create blob URL
+	u, err := url.Parse(f.storageURL + "/" + name)
+	if err != nil {
+		return
+	}
+	blockBlobURL := azblob.NewBlockBlobURL(*u, f.storagePipeline)
+
+	// Download file
+	resp, err := blockBlobURL.Download(context.TODO(), 0, azblob.CountToEnd, azblob.BlobAccessConditions{}, false)
+	if err != nil {
+		if stgErr, ok := err.(azblob.StorageError); !ok {
+			err = fmt.Errorf("network error while downloading the file: %s", err.Error())
+		} else {
+			// Blob checker
+			if stgErr.Response().StatusCode == http.StatusNotFound {
+				err = nil
+				found = false
+				return
+			}
+			err = fmt.Errorf("azure Storage error while downloading the file: %s", stgErr.Response().Status)
+		}
+		return
+	}
+	body := resp.Body(azblob.RetryReaderOptions{
+		MaxRetryRequests: 3,
+	})
+	defer body.Close()
 
 }
