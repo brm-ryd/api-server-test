90f135170e6e20e78c6647ea0f4a8c226717724e
brm_ <brm.ryd@gmail.com>
2020-10-13 14:45:41 +0700

    update go files

--- welcome-route.go
+++ welcome-route.go
@@ -8,4 +8,18 @@ import (
 )
 
 // Welcome page for GET handler
-// func WelcomeHandler(c *gin.Context)
+func WelcomeHandler(c *gin.Context) {
+	c.Header("Content-Type", "text/html")
+	f, err := resourcesBox.Open("welcome-page.html")
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	defer f.Close()
+	_, err = io.Copy(c.Writer, f)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+}
